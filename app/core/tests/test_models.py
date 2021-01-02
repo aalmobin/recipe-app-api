8a2e3ff055e937e41941a7a0db5512d11455f1fa
Mahian <mdmahian580@gmail.com>
2021-01-02 11:54:43 +0600

    space mistake is solved.

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -9,8 +9,8 @@ class ModelTests(TestCase):
         email = 'test@ymail.com'
         password = 'test123'
         user = get_user_model().objects.create_user(
-             email = email,
-             password = password
+             email=email,
+             password=password
         )
 
         self.assertEqual(user.email, email)
